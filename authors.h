d1cc513e9f240a6e48f9b711d06b883ff001383d
Nikhil Sai Pothakamuri <93297514+wubbalubba12@users.noreply.github.com>
2026-02-22 09:54:11 +0100

    Add files via upload

---
+++ authors.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+
+#define AUTHORS_THREE_A "Lukas Rothenberger, someone else"
+#define AUTHORS_THREE_B ""
+
+#define AUTHORS_FOUR_A ""
+#define AUTHORS_FOUR_B ""
+#define AUTHORS_FOUR_C ""
+#define AUTHORS_FOUR_D ""
+
+#define AUTHORS_FIVE_A ""
+#define AUTHORS_FIVE_B ""
+#define AUTHORS_FIVE_C ""
+
+#define AUTHORS_SIX_A ""
+
+#define AUTHORS_SEVEN_A ""
+#define AUTHORS_SEVEN_B ""
+
+#define AUTHORS_EIGHT_A ""
