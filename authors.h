389392112aec27cf21bf1fb60204e3482803df56
Nikhil Sai Pothakamuri <93297514+wubbalubba12@users.noreply.github.com>
2026-02-22 10:16:51 +0100

    Update authors in authors.h

--- authors.h
+++ authors.h
@@ -2,21 +2,22 @@
 
 
 
-#define AUTHORS_THREE_A "Lukas Rothenberger, someone else"
-#define AUTHORS_THREE_B ""
+#define AUTHORS_THREE_A "Finja Pohlenz"
+#define AUTHORS_THREE_B "Finja Pohlenz"
 
-#define AUTHORS_FOUR_A ""
-#define AUTHORS_FOUR_B ""
-#define AUTHORS_FOUR_C ""
-#define AUTHORS_FOUR_D ""
+#define AUTHORS_FOUR_A "Finja Pohlenz"
+#define AUTHORS_FOUR_B "Finja Pohlenz"
+#define AUTHORS_FOUR_C "Finja Pohlenz"
+#define AUTHORS_FOUR_D "Finja Pohlenz"
 
-#define AUTHORS_FIVE_A ""
-#define AUTHORS_FIVE_B ""
-#define AUTHORS_FIVE_C ""
+#define AUTHORS_FIVE_A "Finja Pohlenz, Nikhil Pothakamuri"
+#define AUTHORS_FIVE_B "Finja Pohlenz, Nikhil Pothakamuri"
+#define AUTHORS_FIVE_C "Finja Pohlenz, Nikhil Pothakamuri"
 
-#define AUTHORS_SIX_A ""
+#define AUTHORS_SIX_A "Nikhil Pothakamuri"
 
-#define AUTHORS_SEVEN_A ""
-#define AUTHORS_SEVEN_B ""
+#define AUTHORS_SEVEN_A "Nikhil Pothakamuri"
+#define AUTHORS_SEVEN_B "Nikhil Pothakamuri"
 
-#define AUTHORS_EIGHT_A ""
+
+#define AUTHORS_EIGHT_A "Nikhil Pothakamuri"
