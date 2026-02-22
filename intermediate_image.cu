9c4cdd791e88681c42d149debb4dcbda24e65e82
Nikhil Sai Pothakamuri <93297514+wubbalubba12@users.noreply.github.com>
2026-02-22 10:10:34 +0100

    Add files via upload

---
+++ intermediate_image.cu
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <iostream>
+#include <cuda_runtime_api.h>
+#include "intermediate_image.h"
+#include "gpu_matrix_convolution.h"
+#include "gpu_utilities.h"
+
+void IntermediateImage::apply_sobel_filter(){
+}
