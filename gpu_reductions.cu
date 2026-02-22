65f0872e0b3858853dfdbef063a12648dbbb8b42
Nikhil Sai Pothakamuri <93297514+wubbalubba12@users.noreply.github.com>
2026-02-22 09:57:18 +0100

    Add files via upload

---
+++ gpu_reductions.cu
@@ -0,0 +1,8 @@
+#include "gpu_reductions.h"
+#include <cstdio>
+#include <iostream>
+#include <cuda_runtime_api.h>
+
+double get_max_value(void** d_source_image, std::uint32_t source_image_height, std::uint32_t source_image_width){
+    return 0;
+}
